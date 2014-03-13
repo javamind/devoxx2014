b8ece8e52787a14f9a1bed394bbeecb75d3f6a28
“Guillaume <guillaume.ehret.java@gmail.com>
2014-03-13 18:27:29 +0100

    Ajout test de la couche service

---
+++ src/test/java/com/ninjamind/conference/service/ConferenceHandlerEventTest.java
@@ -0,0 +1,11 @@
+package com.ninjamind.conference.service;
+
+import junit.framework.TestCase;
+
+/**
+ * {@link }
+ *
+ * @author EHRET_G
+ */
+public class ConferenceHandlerEventTest extends TestCase {
+}
