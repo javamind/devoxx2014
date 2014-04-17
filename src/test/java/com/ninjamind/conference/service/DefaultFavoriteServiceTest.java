9e95626c0c84f032b504c8867980621f3f25a091
ehret_g <guillaume.ehret@gmail.com>
2014-04-17 18:27:13 +0200

    last modif I Hope 5

--- src/test/java/com/ninjamind/conference/service/DefaultFavoriteServiceTest.java
+++ src/test/java/com/ninjamind/conference/service/DefaultFavoriteServiceTest.java
@@ -19,6 +19,7 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
+import static junitparams.JUnitParamsRunner.$;
 import static org.junit.Assert.assertEquals;
 import static org.mockito.Mockito.when;
 
