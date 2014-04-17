8541e4ee5477a1b8bd3e0d4bb0f86fbee74f1a21
ehret_g <guillaume.ehret@gmail.com>
2014-04-17 17:45:07 +0200

    last modif I Hope 2

--- src/test/java/com/ninjamind/conference/service/DefaultFavoriteServiceTest.java
+++ src/test/java/com/ninjamind/conference/service/DefaultFavoriteServiceTest.java
@@ -46,8 +46,8 @@ public class DefaultFavoriteServiceTest {
     public void testTheHypestConfOK() throws Exception {
 
         ///////////////  premier cas de test : Devoxx2014 + Mix-IT2014
-        Conference devoxx2014 = new Conference("Devoxx2014", 154L, 658L, 2820L);
-        Conference mixit2014 = new Conference("Mix-IT2014", 30L, 200L, 845L);
+        Conference devoxx2014 = new Conference("Devoxx2014", 154L, 658L);
+        Conference mixit2014 = new Conference("Mix-IT2014", 30L, 200L);
         conferences.add(devoxx2014);
         conferences.add(mixit2014);
 
@@ -62,9 +62,8 @@ public class DefaultFavoriteServiceTest {
 
         /////////////// deuxième cas de test : Devoxx2014 + Mix-IT2014 + JugSummerCamp2014
         conferences.clear();
-        conferences.add(devoxx2014);
         conferences.add(mixit2014);
-        Conference jugsummercamp2014 = new Conference("JugSummerCamp2014", 12L, 97L);
+        Conference jugsummercamp2014 = new Conference("JugSummerCamp2014", 10L, 100L);
         conferences.add(jugsummercamp2014);
 
         when(conferenceRepository.findAll()).thenReturn(conferences);
@@ -73,7 +72,7 @@ public class DefaultFavoriteServiceTest {
         for (Conference conf : theHypestConfs2) {
             confNames2.add(conf.getName());
         }
-        List<String> expected2 = Arrays.asList("JugSummerCamp2014","Mix-IT2014","Devoxx2014");
+        List<String> expected2 = Arrays.asList("JugSummerCamp2014","Mix-IT2014");
         assertEquals(expected2, confNames2);
 
         /////////////// troisième cas de test : Devoxx2014 + Mix-IT2014 sans un parametre
